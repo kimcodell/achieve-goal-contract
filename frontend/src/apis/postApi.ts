2bea37554ceeb5ac6e76a00b9f40d34540ea820d
minhyuk02 <minhyuk@indinity.co>
2022-11-02 19:34:28 +0900

    update

---
+++ frontend/src/apis/postApi.ts
@@ -0,0 +1,12 @@
+import axiosInstance from "@utils/axios";
+import { PostDto, PostSimpleDto } from "@_types/PostDto";
+
+export async function getAllPosts(): Promise<PostSimpleDto[]> {
+  const { data: { data } } = await axiosInstance.get('/v1/post');
+  return data;
+}
+
+export async function getPostById(params: {postId: number}): Promise<PostDto> {
+  const { data: { data } } = await axiosInstance.get(`/v1/post/${params.postId}`);
+  return data;
+}
---
+++ frontend/src/types/types.ts
@@ -0,0 +1,5 @@
+export enum PostStatus {
+  IN_PROGRESS = 1,
+  SUCCESS = 2,
+  FAIL = 3,
+}
